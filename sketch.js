51f61931830eef4a3ea41ee0422dd8481c98f04e
saipsysid <66079077+saipsysid@users.noreply.github.com>
2020-06-20 08:32:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,63 @@
+                                                                                                                                      var ball,img,paddle,computerpaddleimg,computerpaddle;
+function preload() {
+  ballimg = loadImage('ball.png');
+  paddleimg=loadImage("paddle.png");
+  computerpaddleimg=loadImage("paddle.png");
+}
+function setup() {
+  createCanvas(400, 400);
+  ball=createSprite(200,200,20,20);
+  ball.addImage (ballimg); 
+  ball.velocityX=0;  
+  paddle=createSprite(350,200,20,100);
+  paddle.addImage(paddleimg);
+  computerpaddle=createSprite(50,205,20,100)
+  computerpaddle.addImage(computerpaddleimg);
+
+}
+
+function draw() {
+  background(205,153,0);
+  
+  edges=createEdgeSprites();
+  //Bounce Off the Left Edge only
+  ball.bounceOff(edges[0]); 
+  
+  ball.bounceOff(edges[2]);
+  ball.bounceOff(edges[3]);
+  
+  ball.bounceOff(paddle);
+  ball.bounceOff(computerpaddle);
+  
+  
+  
+  
+  paddle.collide(edges);
+  if(keyDown(UP_ARROW))
+  {
+    paddle.y=paddle.y+20;
+  }
+  
+  if(keyDown(DOWN_ARROW))
+  {
+    paddle.x=paddle.y-20;
+  }
+  drawSprites();
+  
+  //giving velocity to the ball 
+ 
+  paddle.y=mouseY;
+  computerpaddle.y=ball.y;
+   
+   if(keyDown('space')){
+     ball.velocityX=3;
+     ball.velocityY=3;   
+     }
+  
+}
+
+function explosion()
+{
+  ball.velocityY=random(-8,8);
+}
+
